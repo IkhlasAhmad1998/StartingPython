3ca20ea0d0fdcfc0eb57114c834dd4d7d0d87677
ikhlasAhmad <ikhlasahmad1998@gmail.com>
2020-10-06 15:59:07 +0500

    added loop file

---
+++ loops.py
@@ -0,0 +1,52 @@
+# for(int x; x<10;x++)
+# {
+#
+# }
+
+
+# name = input("Enter your name: ")
+# length = len(name)
+
+# for x in range(1,10:
+# print(x)
+# for x in name:
+#     print("hi: {}".format(name))
+
+# for x in range(len(name)):
+#     print("hi: {}".format(name))
+#
+
+# String traversing
+
+# uni = "Hazara University"
+
+# print(uni[-4:-1])
+
+
+# for i in range(1,11):
+#     print("2 x {} = {}".format(i, i*2))
+
+
+# Table = int(input("Enter table to print: "))
+# for j in range(1,11):
+#     print("{} x {} = {}".format(Table, j, Table*j))
+
+# result = 1
+# for g in range(9000,0,-1):
+#     result = result * g
+# print(result)
+# r = 1
+# for x in range(0, 100):
+#     factorial = input("Enter number to print factorial: ")
+#     if factorial == "quit":
+#         break
+#     else:
+#         factorial = int(factorial)
+#         r = 1
+#         for f in range(1, factorial+1):
+#             r = r * f
+#         print(r)
+
+
+number = int(input("enter number:"))
+print(number)
